263ea7793b2ad19f377738740f392dde11d8c7fc
hding <helen.ding.uq@gmail.com>
2017-04-26 09:22:44 +1000

    Update App.java

--- src/main/java/xml/stripnamespace/converter/App.java
+++ src/main/java/xml/stripnamespace/converter/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+/** * Marshall XML to byte content without namespace * @param jc JAXBContext * @param element Jaxb Object * @return the byte content */static byte[] marshallToByteNoNames(JAXBContext jc, Object element) {    try {        Marshaller marshaller = jc.createMarshaller();        ByteArrayOutputStream baos = new ByteArrayOutputStream();        OutputStreamWriter writer = new OutputStreamWriter(baos);        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);        marshaller.marshal(element, NoNamesWriterHelper.filter(writer));        return baos.toByteArray();    } catch (Exception ex) {        throw new SystemException(ex.getMessage(), ex);    }}
+    
+    
 }
